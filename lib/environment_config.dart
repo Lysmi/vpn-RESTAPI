3a0885bfdddaafebe961874f930732503d22c315
yukkop <yukkop2@mail.ru>
2022-12-15 18:47:42 +0100

    chore: add own env config; update gitignore; .idea config

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -1,11 +1,11 @@
 class EnvironmentConfig {
   static const String host = 'localhost';
 
-  static const String database = 'vpn-main';
+  static const String database = 'gwp.vpn-main';
 
   static const String username = 'postgres';
 
-  static const String password = '0660838337';
+  static const String password = 'postgres';
 
   static const num port = 5432;
 }
