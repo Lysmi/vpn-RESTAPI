8e1a13d848119605d20fa4a9df5ac5e695fd8763
Lysmi <lysmi.mx@mail.ru>
2023-01-16 22:42:58 +0100

    fix: registered event controller

--- bin/gateway.dart
+++ bin/gateway.dart
@@ -8,6 +8,7 @@ import 'package:shelf_swagger_ui/shelf_swagger_ui.dart';
 import 'package:stormberry/stormberry.dart';
 import 'package:vpn_rest_server/environment_config.dart';
 import 'controllers/config_controller.dart';
+import 'controllers/event_contoller.dart';
 import 'controllers/server_controller.dart';
 import 'controllers/user_controller.dart';
 import 'data/providers/data_provider.dart';
@@ -72,6 +73,7 @@ void main(List<String> args) async {
   ConfigController(router: router).addHandlers();
   UserController(router: router).addHandlers();
   ServerController(router: router).addHandlers();
+  EventController(router: router).addHandlers();
 
   final swaggerHandler = SwaggerUI(swaggerPath, title: 'Swagger Test');
   router.get("/swagger", swaggerHandler.call);
