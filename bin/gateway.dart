498fb1a833bba9c6557452d86a11c0444b52b7ea
Lysmi <lysmi.mx@mail.ru>
2023-01-20 04:17:22 +0100

    fix: port changed

--- bin/gateway.dart
+++ bin/gateway.dart
@@ -103,7 +103,7 @@ void main(List<String> args) async {
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(router);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '8083');
   final server = await serve(handler, ip, port);
   dailyDecreaseBalanceRun();
   print('Server listening on port ${server.port}');
